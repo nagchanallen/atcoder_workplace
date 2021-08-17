565f7b793a0113fabe396fc13980a7e292308a0a
nagchan <52110464+nagchanallen@users.noreply.github.com>
2021-08-18 02:30:29 +0800

    8/18 midnight exercises

--- joi2009ho/B.cpp
+++ joi2009ho/B.cpp
@@ -1,15 +1,32 @@
-#include <iostream>
+#include <bits/stdc++.h>
+#include <atcoder/all>
 using namespace std;
-int D, n, m, d[100005], k[10005], ans = 0;
+using namespace atcoder;
+using ll = long long;
+#define rep0(i, n) for (int i = 0; i < (int)(n); ++i)
+#define rep1(i, n) for(int i = 1; i <= int(n); ++i)
+ll MOD = 1e9+7;
+ll LLINF = 3e18;
+int INTINF = 2e9;
 
+// global variables
+ll ans = 0;
+int D, N, M, d[100005], k[10005];
 int main() {
-    cin >> D; cin >> n; cin >> m;
-    for (int i = 2; i <= n; i++) {
-        cin >> d[i];
+    // input
+    cin >> D; cin >> N; cin >> M;
+    d[0] = 0;
+    d[N] = D;
+    for (int i = 1; i < N; ++i) cin >> d[i];
+    rep0(i, M) cin >> k[i];
+    // solve
+    sort(d, d + (N + 1));
+    rep0(i, M) {
+        int up = lower_bound(d, d + N, k[i]) - d;
+        if (up == 0) continue;
+        int down = up - 1;
+        ans += min(abs(d[up] - k[i]), abs(d[down] - k[i]));
     }
-    for (int i = 1; i <= m; i++) {
-        cin >> k[i];
-    }
-
-
+    // output
+    cout << ans << endl;
 }
