a86ece692c47308e4590e08db6189445e436b0dc
nagchanallen <nagisawags@gmail.com>
2025-01-18 22:43:30 +0900

    Add soft link for generated code folder

--- genABCNewFiles.sh
+++ genABCNewFiles.sh
@@ -5,3 +5,5 @@ filename_array=("A" "B" "C" "D" "E" "F" "G" "H")
 for i in "${filename_array[@]}"; do
     cp cptemplate.cpp "$contest_name/$i.cpp"
 done
+cd "$contest_name"
+ln -s ../atcoder atcoder
